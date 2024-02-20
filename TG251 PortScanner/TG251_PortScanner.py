5e3d696c795991568aae75ae77c66fb4277789d1
Tyler Garner <tylerhearngarner@gmail.com>
2024-02-20 12:22:12 +0000

    argparse has been added instead of manualling creating help and options.

--- TG251 PortScanner/TG251_PortScanner.py
+++ TG251 PortScanner/TG251_PortScanner.py
@@ -1,13 +1,12 @@
-from msilib.schema import Environment
 import socket
-from tokenize import Ignore
-from turtle import clear
+from tracemalloc import start
 import pyfiglet
 from datetime import datetime
 import sys
 import os    
 import argparse
 
+PortDic = ['20','21','22','80','2556']
 # help page
 def exitPage():
     exitcommand = input("Please enter Q to exit this page\n>")
@@ -16,19 +15,6 @@ def exitPage():
          Startup()
     else:
         exitPage() 
-         
-def helpPage():
-     print ("""
-            -- Help Page --
-            Port Scanner will ping ports of the desired target host and display the open ports within the target host.
-            - Commands -
-            -      -H   ping one target host
-            -      -t   ignores the targeted host
-            -      -L   sends the output to a desired text file      
-            -      man  Manual page for options
-            """)
-     exitPage()
-
 def manPage():
      print("""
 --- Manual Page ---
@@ -39,16 +25,24 @@ def Startup():
     BannerText = "Port Scanner"
     AsciiBanner = pyfiglet.figlet_format(BannerText)
     print(AsciiBanner)
-    # Asking user to enter their target host or target file
-    command = input("enter target host or target file\nuse command help for options\n\n>")
     try:
-        if command.lower() == "help": # convert the text from input into a lowercase and check if user has inputed help command
-            helpPage() 
-        else:
-            print("invalid command - see help page or man page\n>")
+        # Help menu using -h - displays brief description of the item
+        # Argument option being created to ensure user has the mandatory options in command
+        parser = argparse.ArgumentParser(prog='PortScanner',description='Scans the target address for open ports',epilog='Text at the bottom of help')
+        parser.add_argument('-p,', dest='port',type=str, help='Accepts one port to scan - if blank will scan common ports')
+        requiredArguemnts = parser.add_mutually_exclusive_group(required=True)
+        requiredArguemnts.add_argument('-H,', dest='host',type=str, help='Accepts one IPV4 address - cannot be used with -t option')
+        requiredArguemnts.add_argument('-t,', dest='file',type=str, help='Reads text file and pings all IPV4 address - cannot be used with -H option')
+        args = parser.parse_args()
+    
+        TargetedAddress = args.host
+        PortAddress = args.port
+        print(TargetedAddress)
+        print(PortAddress)
     except:
-            print("invalid command - see help page or man page\n>")    
- 
+        os.system('cls')
+        Startup()
+
 Startup()
 
 # man page
